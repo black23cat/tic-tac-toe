53f66798a4e1ca6b0ac8a4e9bc4ba524fa0238b2
black2car <black2car+prayogi.pangestu1299@gmail.com.noreply.github.com>
2024-11-14 17:34:51 +0700

    Add Gameboard Module

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Hello World!")
+// Module patter reference from https://www.ayweb.dev/blog/building-a-house-from-the-inside-out
+
+function Gameboard(){
+  //Create 3x3 2d board
+  //Each square on board hold Cell()
+  //Expose markCell method to mark selected cell
+  const board = [];
+  for(let i = 0; i < 3; i++){
+    board[i] = [];
+    for(let j = 0; j < 3; j++){
+      board[i].push(Cell());
+    }
+  }
+  // Render 2d board
+  const getBoard = () => board;
+
+  const markCell = (row, col, marker) => {
+    // check if cell is not marked
+    const availableCells = 
+      (board[row][col].getCellMarker() !== "")? true: false;
+
+    if(availableCells){
+      board[row][col].playerMarker(marker)
+      return true;
+    } else { // if there's already mark on the cell, exit program with returned value
+      return false;
+    }
+  }
+
+  // Render board that have marker on cell
+  const printBoard = () => {
+    const boardWithCellMarker = board.map((row) => row.map((cell) => cell.getCellMarker()));
+    console.log(boardWithCellMarker);
+  };
+
+  return {
+    getBoard,
+    markCell,
+    printBoard
+  }
+}
